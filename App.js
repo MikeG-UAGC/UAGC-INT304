a239e0046c5cbaf110edb2b0485b6b5c3c0749e4
MikeG-UAGC <mike.glogoza0906@gmail.com>
2024-07-08 18:44:52 -0400

    Add files via upload
    Week 3 Updates included

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import EmployeeForm from './Components/EmployeeForm'; 
+import EmployeeForm from './Components/EmployeeForm'; // Adjust the path based on your file structure
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const savedEmployees = JSON.parse(localStorage.getItem('employees')) || [];
+    this.state = {
+      employees: savedEmployees
+    };
+  }
+
+  addEmployee = (employee) => {
+    this.setState((prevState) => {
+      const updatedEmployees = [...prevState.employees, employee];
+      localStorage.setItem('employees', JSON.stringify(updatedEmployees));
+      return { employees: updatedEmployees };
+    });
+  };
+
+  saveData = () => {
+    localStorage.setItem('employees', JSON.stringify(this.state.employees));
+    console.log('Employees data saved to local storage:', this.state.employees);
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Employee Form</h1>
-        <EmployeeForm />
+        <EmployeeForm addEmployee={this.addEmployee} saveData={this.saveData} />
       </div>
     );
   }
